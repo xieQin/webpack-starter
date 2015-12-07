bde06d9a3efcb49115545d34de4a87b80e352f85
cherishhsieh <xieqin1991@gmail.com>
2015-12-07 17:53:49 +0800

    use libs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 require('./main.scss');
 
+var $ = require('jquery');
+var moment = require('moment');
 var sub = require('./sub');
 var app  = document.createElement('div');
 app.innerHTML = '<h1>Hello World</h1>';
 app.appendChild(sub());
 document.body.appendChild(app);
+$('body').append('<p>Look at me ! now is ' + moment().format() + '</p>')
